f63aac6b3c3e2bc54d4d7e9f5cb10e34c85b46f5
aquarell <aquarell.dev@gmail.com>
2022-10-28 15:59:15 +0300

    decided to parse without selenium, but using async requests exploring api

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from libs.sbermarket import SberMarket
+import asyncio
 
 
-def main():
+async def main():
     sber_market = SberMarket()
 
-    sber_market.open_map()
+    retailers = await sber_market.gather_retailer_slugs()
 
-    sber_market.gather_store_links(['53', '28'])
+    cities = ['8']
+
+    info = [(city, retailer) for city in cities for retailer in retailers]
+
+    info = info[:50:]
+
+    stores = []
+
+    store_tasks = [sber_market.get_retailer_stores(inf, stores, len(info)) for inf in info]
+
+    await asyncio.wait(store_tasks)
+
+    categories_tasks = [sber_market.get_store_categories(store, len(stores)) for store in stores]
+
+    await asyncio.wait(categories_tasks)
+
+    sber_market.save_to_json(stores)
+
 
 if __name__ == '__main__':
-    main()
+    asyncio.get_event_loop().run_until_complete(main())
